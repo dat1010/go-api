c341065dad459e97ca125a8685d1088bcb30f86c
David Tanner Jr <tannerdjr@gmail.com>
2025-03-09 22:55:14 -0400

    testing

--- routes/routes.go
+++ routes/routes.go
@@ -1,11 +1,9 @@
 package routes
 
 import (
-	"github.com/dat1010/go-api/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(r *gin.RouterGroup) {
 	r.GET("/healthcheck", controllers.GetHealthCheck)
-	r.GET("/stats/weekly", controllers.GetWeeklyStats)
 }

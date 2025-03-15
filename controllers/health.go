e29aa2faa7d0e0ddebb9acb15cb424a205641f13
David Tanner Jr <tannerdjr@gmail.com>
2025-03-15 15:51:26 -0400

    testing full pipelines

--- controllers/health.go
+++ controllers/health.go
@@ -9,7 +9,7 @@ type Health struct {
 	Version string `json:"version"`
 }
 
-var healthCheckData = Health{Version: "0.0.7"}
+var healthCheckData = Health{Version: "0.0.8"}
 
 func GetHealthCheck(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, healthCheckData)

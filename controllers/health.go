703c9aecfe802183b162d135e50278f7a7fa55d9
Tanner Jr., David Arthur <dtannerjr@bwh.harvard.edu>
2025-03-22 17:24:06 -0400

    added version

--- controllers/health.go
+++ controllers/health.go
@@ -3,13 +3,15 @@ package controllers
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
 )
 
 type Health struct {
 	Version string `json:"version"`
 }
 
-var healthCheckData = Health{Version: "0.0.9"}
+var version = os.Getenv("VERSION")
+var healthCheckData = Health{Version: version}
 
 func GetHealthCheck(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, healthCheckData)

ed4f1ec7190a219a55022593330bdec54c87e3a3
Tanner Jr., David Arthur <dtannerjr@bwh.harvard.edu>
2025-05-03 16:57:53 -0400

    added a lot, may the AI gods look down kindly on us

---
+++ routes/posts.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"github.com/dat1010/go-api/controllers"
+	"github.com/dat1010/go-api/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterPostRoutes(r *gin.RouterGroup) {
+	posts := r.Group("/posts")
+	{
+		// Public routes
+		posts.GET("", controllers.ListPosts)
+		posts.GET("/:id", controllers.GetPost)
+
+		// Protected routes
+		posts.Use(middleware.Auth0())
+		posts.POST("", controllers.CreatePost)
+		posts.PUT("/:id", controllers.UpdatePost)
+		posts.DELETE("/:id", controllers.DeletePost)
+	}
+}

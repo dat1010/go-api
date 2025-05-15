d113af3d6f814ff5fb32061dd6fc6091eaea6ee3
David Tanner Jr <tannerdjr@gmail.com>
2025-05-15 19:00:23 -0400

    testing"
    "

--- controllers/eventy.go
+++ controllers/eventy.go
@@ -3,5 +3,13 @@ package controllers
 import "github.com/gin-gonic/gin"
 
 // @Summary Create a new event
+// @Description Create a eventbridge the provided data
+// @Accept json
+// @Produce json
+// @Param post body controllers.CreateEventRequest true "EvenPostt data"
+// @Success 201 {object} controllers.Event
+// @Failure 500 {object} object "Internal server error"
+// @Router /event [post]
+
 func CreateEvent(c *gin.Context) {
 }
